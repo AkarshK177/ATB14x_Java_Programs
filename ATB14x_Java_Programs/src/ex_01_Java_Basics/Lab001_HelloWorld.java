b7cc3b13082768f8058975d6d83afb011892c35e
AkarshK177 <akarsh_17@outlook.com>
2025-09-21 01:30:55 +0530

    Lab001_HelloWorld.java

---
+++ ATB14x_Java_Programs/src/ex_01_Java_Basics/Lab001_HelloWorld.java
@@ -0,0 +1,12 @@
+package ex_01_Java_Basics;
+
+public class Lab001_HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.print("Hello1 ");
+		System.out.println("Hello2");
+		System.out.print("Hello3 ");
+		System.out.println("Hello4");
+	}
+
+}

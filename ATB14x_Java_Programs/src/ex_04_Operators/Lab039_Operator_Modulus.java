b8e5b1f892f53aa24f9f4479f0be1421e9230038
AkarshK177 <akarsh_17@outlook.com>
2025-10-02 00:38:57 +0530

    Relocate

---
+++ ATB14x_Java_Programs/src/ex_04_Operators/Lab039_Operator_Modulus.java
@@ -0,0 +1,13 @@
+package ex_04_Operators;
+
+public class Lab039_Operator_Modulus {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 10;
+        System.out.println(a%b); // % modulus
+        
+//          Number % 2 -> R-> 1 -> odd , 0 -> even
+
+
+    }
+}

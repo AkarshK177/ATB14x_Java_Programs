8a36a27016dc10045db1532e3f055b4e0fd5e3a2
AkarshK177 <akarsh_17@outlook.com>
2025-10-11 11:53:51 +0530

    21st Sep Programs - II

--- ATB14x_Java_Programs/src/ex_07_Increment_Decrement_Op/Lab074_Advance_ID.java
+++ ATB14x_Java_Programs/src/ex_07_Increment_Decrement_Op/Lab074_Advance_ID.java
@@ -4,12 +4,7 @@ public class Lab074_Advance_ID {
     public static void main(String[] args) {
         int a = 10;
         System.out.println(a++ + a);
-        // D & C
-        //  A + B
-        // A -> a++
-        // +
-        // B
-
+       
         // ERT
         // LNo |  a | Exp
         //  5  | 10 | NA
--- ATB14x_Java_Programs/src/ex_07_Increment_Decrement_Op/Lab075_Exp1.java
+++ ATB14x_Java_Programs/src/ex_07_Increment_Decrement_Op/Lab075_Exp1.java
@@ -1,4 +1,3 @@
-
 package ex_07_Increment_Decrement_Op;
 
 public class Lab075_Exp1 {

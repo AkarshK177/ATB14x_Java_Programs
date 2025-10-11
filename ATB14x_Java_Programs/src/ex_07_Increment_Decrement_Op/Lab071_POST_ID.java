41f1ea694debbf3ae546c251c018434ac64d7edb
AkarshK177 <akarsh_17@outlook.com>
2025-10-11 11:50:14 +0530

    21st Sep Programs

---
+++ ATB14x_Java_Programs/src/ex_07_Increment_Decrement_Op/Lab071_POST_ID.java
@@ -0,0 +1,16 @@
+package ex_07_Increment_Decrement_Op;
+
+public class Lab071_POST_ID {
+
+	public static void main(String[] args) {
+		
+		int post_a = 10;
+		int b = post_a++;
+		
+		System.out.println("a = " + post_a++);
+		System.out.println("b = " + b);
+		
+
+	}
+
+}
